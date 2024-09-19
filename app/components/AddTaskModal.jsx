d03be498d9fa48c06300a8ed86946630e2da3efe
Wesley <wes.lol25@gmail.com>
2024-09-19 14:25:42 -0300

    style: changing style to add task modal

--- app/components/AddTaskModal.jsx
+++ app/components/AddTaskModal.jsx
@@ -3,6 +3,7 @@ import styles from "../styles/Modal.module.scss";
 export const AddTaskModal = ({ isOpen, onClose, onAddTask, newTaskTitle, setNewTaskTitle }) => {
     if (!isOpen) return null;
 
+    // add task function
     const handleAddTask = () => {
         if (newTaskTitle.trim()) {
             onAddTask(newTaskTitle);
@@ -15,15 +16,19 @@ export const AddTaskModal = ({ isOpen, onClose, onAddTask, newTaskTitle, setNewT
         <div className={styles.modalOverlay}>
             <div className={styles.modalContent}>
                 <h2>Nova Tarefa</h2>
+
+                <label htmlFor="">Título</label>
+
                 <input
                     type="text"
                     value={newTaskTitle}
                     onChange={(e) => setNewTaskTitle(e.target.value)}
-                    placeholder="Título da tarefa"
+                    placeholder="Digite"
                 />
+
                 <div className={styles.modalActions}>
-                    <button onClick={handleAddTask}>Adicionar</button>
-                    <button onClick={onClose}>Cancelar</button>
+                    <button className={styles.cancelButton} onClick={onClose}>Cancelar</button>
+                    <button className={styles.addTaskButton} onClick={handleAddTask}>Adicionar</button>
                 </div>
             </div>
         </div>
